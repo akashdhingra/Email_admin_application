1a5cfa49dfcfb5839418e5a7228b5740b3a8bd55
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-03 13:47:51 -0400

    Initial commit

---
+++ src/Email.java
@@ -0,0 +1,12 @@
+public class Email {
+
+    // variable are kept private so that people cannnot access it directly.
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String Department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+
+}
