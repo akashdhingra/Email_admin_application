b9dfb1e2ac515cf812c0a2f9d3a7ef3d993e1555
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-03 13:50:57 -0400

    Checking the push operation

--- src/EmailApp.java
+++ src/EmailApp.java
@@ -3,7 +3,7 @@ public class EmailApp {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello");
+        System.out.println("Hello, This is an email admin project");
 
     }
 
