9fd7c7e9ea9bbe73b17c8d2fbb72f0c56ac314a8
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-03 16:19:10 -0400

    implemented setdepartment and mail box capacity functions

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 
     // variable are kept private so that people cannot access it directly.
@@ -7,7 +9,7 @@ public class Email {
     private String lastName;
     private String password;
     private String Department;
-    private int mailboxCapacity;
+    private int mailboxCapacity = 500;
     private String alternateEmail;
 
     // Constructor to receive first name and last name
@@ -16,13 +18,35 @@ public class Email {
         this.firstName = firstname;
         this.lastName = lastname;
         System.out.println("Name is " + this.firstName + " " + this.lastName);
+
+        this.Department = setDepartment();
+        System.out.println("Department: " + this.Department);
+        System.out.println("Mail Box capacity: " + setMailBoxCapacity() + " MB");
     }
 
     // Generate a random password
 
     // Ask for the department
+    private String setDepartment()
+    {
+        System.out.println("Please enter the department code:\n1: Sales\n2: Development\n3: Accounting\n0: None");
+        Scanner sc = new Scanner(System.in);
+        int code = sc.nextInt();
+        if(code == 1)
+            return "Sales";
+        else if(code == 2)
+            return "Development";
+        else if (code == 3)
+            return "Accounting";
+        else
+            return "None";
+    }
 
     // Set the mailbox capacity
+    private int setMailBoxCapacity()
+    {
+        return this.mailboxCapacity;
+    }
 
     // Set the alternative email
 
