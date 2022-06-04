c6bff9aa263382cccdbceb1a86d9b4d886a9ef38
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-04 17:17:18 -0400

    implemented scanner for user input

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -6,7 +6,15 @@ public class EmailApp{
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        Email em1 = new Email("Akash","Dhingra");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter for first name: ");
+        String firstname = sc.next();
+
+        System.out.println("Please enter for last name: ");
+        String lastname = sc.next();
+
+        Email em1 = new Email(firstname,lastname);
     }
 
 }
